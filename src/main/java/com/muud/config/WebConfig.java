af22c9f5293120688bb4010cf7b8949a50bc5937
taehyeon <97taehyun@gmail.com>
2024-02-21 21:09:04 +0900

    feature: CORS 이슈 해결을 위한 config 추가 #18

---
+++ src/main/java/com/muud/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.muud.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*");
+    }
+}
