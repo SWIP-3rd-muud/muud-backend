74d25a5b2a00331c6871fde485a9cf4ca685bfec
taehyeon <97taehyun@gmail.com>
2024-02-23 14:49:56 +0900

    feature: 유효성 검증 어노테이션 예외 처리 #33

--- src/main/java/com/muud/global/error/ExceptionType.java
+++ src/main/java/com/muud/global/error/ExceptionType.java
@@ -6,7 +6,8 @@ public enum ExceptionType {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "로그인이 필요한 서비스입니다."),
     FORBIDDEN_USER(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 유효하지 않습니다.");
 
     private final HttpStatus status;
     private String message;
