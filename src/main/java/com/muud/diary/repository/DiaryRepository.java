c7a9a426af4c5891420f34480d3b0d01ee50457c
taehyeon <97taehyun@gmail.com>
2024-02-24 16:04:37 +0900

    feature: 월별 일기 조회 기능 구현 #39

--- src/main/java/com/muud/diary/repository/DiaryRepository.java
+++ src/main/java/com/muud/diary/repository/DiaryRepository.java
@@ -2,8 +2,14 @@ package com.muud.diary.repository;
 
 import com.muud.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    @Query("SELECT d FROM Diary d WHERE MONTH(d.createdDate) = :month")
+    List<Diary> findByMonth(@Param("month") int month);
 }
