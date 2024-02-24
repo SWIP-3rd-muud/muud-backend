4778635d522412e1604a78acf888c367cf21b877
gagle1231 <kke1231@naver.com>
2024-02-24 21:31:58 +0900

    feature: 예외 타입 추가

--- src/main/java/com/muud/global/error/ExceptionType.java
+++ src/main/java/com/muud/global/error/ExceptionType.java
@@ -9,8 +9,8 @@ public enum ExceptionType {
     NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 유효하지 않습니다."),
 
-    INVALID_AUTHENTICATE(HttpStatus.UNAUTHORIZED, "유효하지 않은 이메일 주소 또는 비밀번호입니다.");
-
+    INVALID_AUTHENTICATE(HttpStatus.UNAUTHORIZED, "유효하지 않은 이메일 주소 또는 비밀번호입니다."),
+    NOT_REGISTERED(HttpStatus.FORBIDDEN, "회원가입이 필요합니다");
     private final HttpStatus status;
     private String message;
 
