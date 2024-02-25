fbcc42ababc27eba916e85a3c3a62ef8449189b4
gagle1231 <kke1231@naver.com>
2024-02-25 23:23:28 +0900

    feature: dto validation 추가

--- src/main/java/com/muud/auth/dto/SigninRequest.java
+++ src/main/java/com/muud/auth/dto/SigninRequest.java
@@ -1,9 +1,14 @@
 package com.muud.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class SigninRequest {
+    @NotBlank @Email
     private String email;
+    @NotBlank @Size(min = 8, max = 15)
     private String password;
 }
