fe8729e5c42a0a0d5d1ffc81a3b342c3018b6c32
taehyeon <97taehyun@gmail.com>
2024-03-05 18:47:18 +0900

    fix: 북마크와 로그인 유저 정보 연동 #80

--- src/main/java/com/muud/bookmark/domain/dto/BookmarkRequest.java
+++
@@ -1,4 +0,0 @@
-package com.muud.bookmark.domain.dto;
-
-public record BookmarkRequest(Long userId) {
-}
--- src/main/java/com/muud/bookmark/presentation/BookmarkController.java
+++ src/main/java/com/muud/bookmark/presentation/BookmarkController.java
@@ -1,9 +1,10 @@
 package com.muud.bookmark.presentation;
 
+import com.muud.auth.jwt.Auth;
 import com.muud.bookmark.application.BookmarkService;
 import com.muud.bookmark.domain.Bookmark;
-import com.muud.bookmark.domain.dto.BookmarkRequest;
 import com.muud.bookmark.domain.dto.BookmarkResponse;
+import com.muud.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,29 +17,33 @@ import java.util.List;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
+    @Auth
     @GetMapping("/diaries/bookmarks")
-    public ResponseEntity<List<BookmarkResponse>> getBookmarkResponseList(@RequestBody BookmarkRequest bookmarkRequest) {
-        return ResponseEntity.ok(bookmarkService.getBookmarkResponseListByUser(bookmarkRequest.userId()));
+    public ResponseEntity<List<BookmarkResponse>> getBookmarkResponseList(@RequestAttribute("user") User user) {
+        return ResponseEntity.ok(bookmarkService.getBookmarkResponseListByUser(user.getId()));
     }
 
+    @Auth
     @GetMapping("/diaries/{diaryId}/bookmarks")
-    public ResponseEntity<Boolean> checkBookmark(@PathVariable("diaryId") Long diaryId,
-                                                 @RequestBody BookmarkRequest bookmarkRequest) {
-        boolean isBookmarked = bookmarkService.isBookmarkedByUser(bookmarkRequest.userId(), diaryId);
+    public ResponseEntity<Boolean> checkBookmark(@RequestAttribute("user") User user,
+                                                 @PathVariable("diaryId") Long diaryId) {
+        boolean isBookmarked = bookmarkService.isBookmarkedByUser(user.getId(), diaryId);
         return ResponseEntity.ok(isBookmarked);
     }
 
+    @Auth
     @PostMapping("/diaries/{diaryId}/bookmarks")
-    public ResponseEntity<Object> addBookmark(@PathVariable("diaryId") Long diaryId,
-                                              @RequestBody BookmarkRequest bookmarkRequest) {
-        Bookmark bookmark = bookmarkService.addBookmark(bookmarkRequest.userId(), diaryId);
+    public ResponseEntity<Object> addBookmark(@RequestAttribute("user") User user,
+                                              @PathVariable("diaryId") Long diaryId) {
+        Bookmark bookmark = bookmarkService.addBookmark(user.getId(), diaryId);
         return ResponseEntity.created(URI.create("/bookmarks/"+bookmark.getId())).build();
     }
 
+    @Auth
     @DeleteMapping("/diaries/{diaryId}/bookmarks")
-    public ResponseEntity<Object> removeBookmark(@PathVariable("diaryId") Long diaryId,
-                                                 @RequestBody BookmarkRequest bookmarkRequest) {
-        bookmarkService.removeBookmark(bookmarkRequest.userId(), diaryId);
+    public ResponseEntity<Object> removeBookmark(@RequestAttribute("user") User user,
+                                                 @PathVariable("diaryId") Long diaryId) {
+        bookmarkService.removeBookmark(user.getId(), diaryId);
         return ResponseEntity.noContent().build();
     }
 }
