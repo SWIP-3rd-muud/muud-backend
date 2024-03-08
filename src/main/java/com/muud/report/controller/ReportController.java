ccf390b98f8f6b26f8c4be88096a7bef4d380144
taehyeon <97taehyun@gmail.com>
2024-03-08 18:01:23 +0900

    feature: 월간 리포트 기능 구현 #81

---
+++ src/main/java/com/muud/report/controller/ReportController.java
@@ -0,0 +1,25 @@
+package com.muud.report.controller;
+
+import com.muud.auth.jwt.Auth;
+import com.muud.report.dto.ReportResponse;
+import com.muud.report.service.ReportService;
+import com.muud.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.YearMonth;
+
+@RestController
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportService reportService;
+
+    @Auth
+    @GetMapping("/reports")
+    public ResponseEntity<ReportResponse> getReportResponse(@RequestAttribute("user") User user,
+                                                            @RequestParam(name = "date", required = false) YearMonth inputDate) {
+        YearMonth date = (inputDate != null) ? inputDate : YearMonth.now();
+        return ResponseEntity.ok(reportService.generate(user, date));
+    }
+}
