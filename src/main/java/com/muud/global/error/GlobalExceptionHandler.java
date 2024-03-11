5b4085896ac2889a3264f99aa8a3e7a48726df3a
taehyeon <97taehyun@gmail.com>
2024-03-11 20:12:11 +0900

    fix: 예외처리시 error 레벨 로그 발생하도록 변경 #148

--- src/main/java/com/muud/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/muud/global/error/GlobalExceptionHandler.java
@@ -1,21 +1,27 @@
 package com.muud.global.error;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     protected ResponseEntity<ResponseError> handleGlobalException(ApiException e) {
+        log.error(e.getMessage(), e);
         return ResponseEntity.status(e.getExceptionType().getStatus())
                 .body(new ResponseError(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ResponseError> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    protected ResponseEntity<ResponseError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error(e.getMessage(), e);
         return ResponseEntity.status(ExceptionType.INVALID_INPUT_VALUE.getStatus())
-                .body(new ResponseError(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
+                .body(new ResponseError(e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
     }
 }
