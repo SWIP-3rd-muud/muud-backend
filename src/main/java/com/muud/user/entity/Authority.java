0998a962412d662d2472314ab2f6b3f99cb17100
gagle1231 <kke1231@naver.com>
2024-09-14 16:05:34 +0900

    fix: role name security 형식으로 반환

--- src/main/java/com/muud/user/entity/Authority.java
+++ src/main/java/com/muud/user/entity/Authority.java
@@ -3,10 +3,12 @@ package com.muud.user.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Authority implements GrantedAuthority {
+
     ADMIN, USER;
 
     @Override
     public String getAuthority() {
-        return name();
+        return "ROLE_"+ name();
     }
+
 }
