e909a42b728b02a3eec99ff3516fe7d9f7e50309
taehyeonn <97taehyun@gmail.com>
2024-09-28 00:26:17 +0900

    test: 북마크 제거, 조회 메서드 테스트 코드 작성 #191

--- src/test/java/com/muud/bookmark/service/BookmarkServiceTest.java
+++ src/test/java/com/muud/bookmark/service/BookmarkServiceTest.java
@@ -1,6 +1,7 @@
 package com.muud.bookmark.service;
 
 import com.muud.bookmark.domain.Bookmark;
+import com.muud.bookmark.domain.dto.BookmarkResponse;
 import com.muud.bookmark.repository.BookmarkRepository;
 import com.muud.diary.domain.Diary;
 import com.muud.diary.domain.dto.DiaryRequest;
@@ -18,12 +19,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.*;
 
 import static com.muud.bookmark.exception.BookmarkErrorCode.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookmarkServiceTest {
@@ -116,5 +117,73 @@ class BookmarkServiceTest {
         // when & then
         assertThrows(BOOKMARK_ALREADY_EXISTS.defaultException().getClass(),
                 () -> bookmarkService.addBookmark(user.getId(), diary.getId()));
+    }
+
+
+    @Test
+    @DisplayName("북마크 제거 성공")
+    void removeBookmark_success() {
+        // given
+        Bookmark bookmark = new Bookmark(user, diary);
+        when(bookmarkRepository.findByUserIdAndDiaryId(user.getId(), diary.getId()))
+                .thenReturn(Optional.of(bookmark));
+
+        // when
+        bookmarkService.removeBookmark(user.getId(), diary.getId());
+
+        // then
+        verify(bookmarkRepository, times(1)).delete(bookmark);
+    }
+
+    @Test
+    @DisplayName("북마크 제거 실패 - 북마크가 존재하지 않는 경우 예외 던지기")
+    void removeBookmark_fail_bookmarkNotFound() {
+        // given
+        when(bookmarkRepository.findByUserIdAndDiaryId(user.getId(), diary.getId()))
+                .thenReturn(Optional.empty());
+
+        // when & then
+        assertThrows(BOOKMARK_NOT_FOUND.defaultException().getClass(),
+                () -> bookmarkService.removeBookmark(user.getId(), diary.getId()));
+
+        verify(bookmarkRepository, never()).delete(any());
+    }
+
+    @Test
+    @DisplayName("유저별 북마크 리스트 조회 성공")
+    void getBookmarkList_success() {
+        // given
+        Bookmark bookmark1 = new Bookmark(user, diary);
+
+        Diary diary2 = Diary.of(user, diaryRequest, null, playList);
+        ReflectionTestUtils.setField(diary, "id", 2L);
+        Bookmark bookmark2 = new Bookmark(user, diary2);
+
+        List<Bookmark> bookmarkList = Arrays.asList(bookmark1, bookmark2);
+
+        when(bookmarkRepository.findByUserId(user.getId())).thenReturn(bookmarkList);
+
+        // when
+        List<BookmarkResponse> response = bookmarkService.getBookmarkList(user.getId());
+
+        // then
+        assertNotNull(response);
+        assertEquals(bookmarkList.size(), response.size());
+        assertEquals(BookmarkResponse.from(bookmark1), response.get(0));
+        assertEquals(BookmarkResponse.from(bookmark2), response.get(1));
+    }
+
+    @Test
+    @DisplayName("유저별 북마크 리스트 조회 성공 - 아무것도 북마크 하지 않은 경우")
+    void getBookmarkList_success_empty() {
+        // given
+        when(bookmarkRepository.findByUserId(user.getId())).thenReturn(Collections.emptyList());
+
+        // when
+        List<BookmarkResponse> response = bookmarkService.getBookmarkList(user.getId());
+
+        // then
+        assertNotNull(response);
+        assertTrue(response.isEmpty());
     }
 }
